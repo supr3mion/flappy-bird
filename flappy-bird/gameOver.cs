d05f56b862fa911699ae63a755c90de63760beaf
supr3mion <230690@edu.rocfriesepoort.nl>
2021-03-17 10:32:08 +0100

    scores

--- flappy-bird/gameOver.cs
+++ flappy-bird/gameOver.cs
@@ -31,15 +31,15 @@ namespace flappy_bird
 
             score = totalScore;
 
-            place1();
+            place1(1);
             
             if (score < first)
             {
-                place2();
+                place1(2);
 
                 if (score < second)
                 {
-                    place3();
+                    place1(3);
 
                     if (score < third)
                     {
@@ -108,11 +108,11 @@ namespace flappy_bird
             }
         }
 
-        private int place1()
+        private int place1(int place)
         {
             OpenConnection();
 
-            string sqlQuery = "SELECT * FROM apparaten WHERE place = " + 1;
+            string sqlQuery = "SELECT * FROM apparaten WHERE place = " + place;
 
             if (this.OpenConnection() == true)
             {
@@ -128,48 +128,5 @@ namespace flappy_bird
             CloseConnection();
             return first;
         }
-
-        private int place2()
-        {
-            OpenConnection();
-
-            string sqlQuery = "SELECT * FROM apparaten WHERE place = " + 2;
-
-            if (this.OpenConnection() == true)
-            {
-                MySqlCommand cmd = new MySqlCommand(sqlQuery, connection);
-
-                MySqlDataReader dataReader = cmd.ExecuteReader();
-
-                if (dataReader.Read())
-                {
-                    int second = Convert.ToInt32(dataReader["score"] + "");
-                }
-            }
-            CloseConnection();
-            return second;
-        }
-
-        private int place3()
-        {
-            OpenConnection();
-
-            string sqlQuery = "SELECT * FROM apparaten WHERE place = " + 3;
-
-            if (this.OpenConnection() == true)
-            {
-                MySqlCommand cmd = new MySqlCommand(sqlQuery, connection);
-
-                MySqlDataReader dataReader = cmd.ExecuteReader();
-
-                if (dataReader.Read())
-                {
-                    int third = Convert.ToInt32(dataReader["score"] + "");
-                }
-            }
-            CloseConnection();
-            return third;
-        }
-
     }
 }
