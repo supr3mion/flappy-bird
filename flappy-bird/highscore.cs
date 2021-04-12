358c310b029b28ffe7de00cafa5d605d0a25b7ff
supr3mion <230690@edu.rocfriesepoort.nl>
2021-04-12 10:04:17 +0200

    code comments voor highscore scherm en start scherm toegevoegd

--- flappy-bird/highscore.cs
+++ flappy-bird/highscore.cs
@@ -16,35 +16,47 @@ namespace flappy_bird
 
         private MySqlConnection connection;
 
+        //hier word de standaard waarde ingestelt
         private int placement = 1;
 
         public highscore()
         {
+            //hier word het form delen geactieveerd
             InitializeComponent();
-
             InitializeDatabaseConnection();
 
+            //hier word de text gereset
             lblHighScoresName.Text = "";
             lblHighScoresScore.Text = "";
 
+            //hier word een sql gemaakt op de top 10 scores op te halen
             string sqlQuery = "SELECT * FROM highscore ORDER BY score DESC LIMIT 10";
 
+            //hier word er gekeken als er een connectie is met de database
             if (this.OpenConnection() == true)
             {
-
+                //hier word de qls uitgevoerd
                 MySqlCommand cmd = new MySqlCommand(sqlQuery, connection);
 
+                //hier word er een reader er opgezet
                 MySqlDataReader dataReader = cmd.ExecuteReader();
 
+                //het volgende stuk word herhaald tot dat de reader niks meer leest, vanwege de sql is dit 10
                 while (dataReader.Read())
                 {
+                    //hier worden de naam en de score van de speler opgehaald en in een label gezet in het highscores scherm
+                    //heir word ook de als bestaande text + de nieuwe text gedaan en zo word er een lijst gemaakt van alle spelers die een highscore hebben
                     lblHighScoresName.Text = lblHighScoresName.Text + placement + "st place: " + dataReader["name"] + "\n\r";
                     lblHighScoresScore.Text = lblHighScoresScore.Text + " met een score van: " + dataReader["score"] + "\n\r";
+
+                    //hier word de plaats +1 gedaan van de de volgende speler (de standaard waarde is 1)
                     placement++;
                 }
 
+                //hier word de datareader afgesloten
                 dataReader.Close();
 
+                //hier word de database connectie verbroken
                 this.CloseConnection();
 
             }
@@ -52,11 +64,13 @@ namespace flappy_bird
 
         private void home_Click(object sender, EventArgs e)
         {
+            //als er op deze afbeelding/knop word geklikt dan word dit scherm afgesloten
             Close();
         }
 
         private void InitializeDatabaseConnection()
         {
+            //hier word database informatie ingevoerd
             string server = "localhost";
             string database = "Flappy-bird";
             string dbUsername = "root";
@@ -70,6 +84,7 @@ namespace flappy_bird
 
         private bool OpenConnection()
         {
+            //hier word de database connectie gemaakt
 
             CloseConnection();
 
@@ -95,6 +110,8 @@ namespace flappy_bird
         }
         private bool CloseConnection()
         {
+
+            //hier word de database connectie gesloten
             try
             {
                 connection.Close();
--- flappy-bird/start.cs
+++ flappy-bird/start.cs
@@ -19,14 +19,24 @@ namespace flappy_bird
 
         private void pbStart_Click(object sender, EventArgs e)
         {
+
+            this.Hide();
+
+            //als er op deze afbeelding/knop word geklikt dan word het spel gestart, er worden geen waarden mee gegeven
+            //ook word dit scherm verborgen door de this.Hide() functie
             mainScreen game = new mainScreen();
             game.ShowDialog();
+
+            //als het game scherm is afgesloten dan wor dit scherm weer zichtbaar gemaakt
+            this.Show();
         }
 
         private void start_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
         {
+            //als de spatie knop is ingedrukt dan word de game gestart
             if (e.KeyCode == Keys.Space)
             {
+                //ook hier word er geen waarde mee gegeven naar de game en de manier van het statren is het zelfde als in de void hierboven
                 this.Hide();
 
                 mainScreen game = new mainScreen();
@@ -38,14 +48,19 @@ namespace flappy_bird
 
         private void pictureBox4_Click(object sender, EventArgs e)
         {
+            //als er op deze afbeelding/knop word geklikt dan word dit scherm verborgen
             this.Hide();
 
+            //het laad scherm word geactiveerd met de waarde "highscores"
             loading loadingScreen = new loading("highscores");
             loadingScreen.Show();
 
+            //het scherm met alle highscores word geladen
             highscore highscoreForm = new highscore();
             highscoreForm.ShowDialog();
 
+            //als het highscores scherm word afgesloten dan stuit het laad scherm ook automatisch
+            //en word dit scherm weer zichtbaar gemaakt
             loadingScreen.Close();
 
             this.Show();
